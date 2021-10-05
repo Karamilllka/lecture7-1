ba54b26e4462ce5538ea8a25c9ab90a026051116
Ekaterina Miheeva <karamilllka@gmail.com>
2021-10-05 11:22:50 +0600

    added test2

--- src/test/java/ru/netology/stats/StatisticServiceTest.java
+++ src/test/java/ru/netology/stats/StatisticServiceTest.java
@@ -10,7 +10,17 @@ class StatisticServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;long actual = service.findMax(incomesInBillions);
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxe() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
 }
